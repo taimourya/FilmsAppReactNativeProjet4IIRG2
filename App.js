b6502c9bbca6cba2bdb2676c97764c04c179dabd
taimourya <taimouriya@gmail.com>
2021-04-14 23:30:59 +0200

    Ajout Bottom navigation

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Image} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Search from './Components/Search';
@@ -7,19 +8,57 @@ import { Provider } from 'react-redux'
 import Store from './Store/configureStore'
 import FilmTrailer from './Components/FilmTrailer';
 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Favorites from './Components/Favorites';
+import { Ionicons } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
+
+
+function SearchStackNavigation() {
+  return (
+    <Stack.Navigator initialRouteName="Search">
+      <Stack.Screen name="Search" component={Search} />        
+      <Stack.Screen name="FilmDetails" component={FilmDetails} />
+      <Stack.Screen name="FilmTrailer" component={FilmTrailer} />
+    </Stack.Navigator>
+  );
+}
+
+
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <Provider store={Store}>
-      <NavigationContainer>
-          <Stack.Navigator initialRouteName="Search">
-            <Stack.Screen name="Search" component={Search} />        
-            <Stack.Screen name="FilmDetails" component={FilmDetails} />
-            <Stack.Screen name="FilmTrailer" component={FilmTrailer} />
-          </Stack.Navigator>
-      </NavigationContainer>
+        <NavigationContainer>
+            <Tab.Navigator
+              screenOptions={({ route }) => ({
+                  tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+        
+                    if (route.name === 'Search') {
+                      iconName = 'search-outline'
+                    } else if (route.name === 'Favorites') {
+                      iconName = 'heart-outline';
+                    }
+        
+                    // You can return any component that you like here!
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                  },
+              })}
+              tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+                showLabel: true,
+                showIcon: true,
+              }}            
+            >
+              <Tab.Screen name="Search" component={SearchStackNavigation}/>
+              <Tab.Screen name="Favorites" component={Favorites} />
+            </Tab.Navigator>
+        </NavigationContainer>   
     </Provider>
   );
 };
---
+++ Components/Favorites.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+
+export default class Favorites extends React.Component {
+    
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <Text>Favorite component</Text>
+            </View>
+        );
+    }
+}
